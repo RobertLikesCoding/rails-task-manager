c000937473a9138ab0926349a2adbb7e700a97ea
Robert Baufeld <rbaufeld@posteo.de>
2024-05-14 14:51:36 +0200

    CRUD complete but checkboxes are still not updating properly

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,6 +7,31 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def new
+    @empty_task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to list_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+    redirect_to find_path(task)
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to list_path
+  end
+
   private
 
   def task_params
